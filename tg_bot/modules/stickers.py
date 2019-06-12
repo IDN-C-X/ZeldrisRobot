df45540d2b71ddd1c4e5be4a400a9a8394663921
Rhyse Simpson <skittles9832@gmail.com>
2019-06-12 10:18:44 +1000

    future proof pack making for when multikang is added

--- tg_bot/modules/stickers.py
+++ tg_bot/modules/stickers.py
@@ -47,8 +47,8 @@ def getsticker(bot: Bot, update: Update):
 def kang(bot: Bot, update: Update, args: List[str]):
     msg = update.effective_message
     user = update.effective_user
-    hash = hashlib.sha1(bytearray(user.id)).hexdigest()
-    packname = "a" + hash[:20] + "_by_"+bot.username
+    packnum = "1"
+    packname = packnum + "_" + str(user.id) + "_by_"+bot.username
     kangsticker = "kangsticker.png"
     if msg.reply_to_message:
         if msg.reply_to_message.sticker:
@@ -111,7 +111,7 @@ def kang(bot: Bot, update: Update, args: List[str]):
         try:
             try:
                 urlemoji = msg.text.split(" ")
-                png_sticker = urlemoji[1] 
+                png_sticker = urlemoji[1]
                 sticker_emoji = urlemoji[2]
             except IndexError:
                 sticker_emoji = "🤔"
@@ -162,8 +162,8 @@ def kang(bot: Bot, update: Update, args: List[str]):
 def makepack_internal(msg, user, png_sticker, emoji, bot):
     name = user.first_name
     name = name[:50]
-    hash = hashlib.sha1(bytearray(user.id)).hexdigest()
-    packname = f"a{hash[:20]}_by_{bot.username}"
+    packnum = "1"
+    packname = f"{packnum}_{str(user.id)}_by_{bot.username}"
     try:
         success = bot.create_new_sticker_set(user.id, packname, name + "'s kang pack",
                                              png_sticker=png_sticker,
