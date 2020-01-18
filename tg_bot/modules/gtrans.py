de21540de72bcec70d3172bf5769f397010ca2b0
ŞŦΔŘŘ¥ <58552395+starry69@users.noreply.github.com>
2020-01-18 23:58:18 +0530

    Update gtrans.py

--- tg_bot/modules/gtrans.py
+++ tg_bot/modules/gtrans.py
@@ -23,7 +23,7 @@ def gtrans(bot: Bot, update: Update, args: List[str]):
         results = translated.text
         lol.reply_text("Translated from {} to {}.\n {}".format(oof, oky, results))
     except :
-        lol.reply_text("Which language should i translate into?")
+        lol.reply_text("ERROR! Try to use `/tl` instead.")
 
 
 __help__ = """- /tr <lang> - To translate to your language!
