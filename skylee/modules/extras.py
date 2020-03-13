bceec7283ec893cea74cb542362183909fdb4f65
starry69 <starry369126@outlook.com>
2020-03-13 14:17:13 +0530

    Remove rlg command from extras // useless af
    Signed-off-by: starry69 <starry369126@outlook.com>

--- skylee/modules/extras.py
+++ skylee/modules/extras.py
@@ -14,13 +14,13 @@ from skylee.modules.disable import DisableAbleCommandHandler
 ABUSE_STRINGS = (
     "Fuck off",
     "Stfu go fuck yourself",
-       "you noob",
-	"Relax your Rear,ders nothing to fear,The Rape train is finally here",
-	"Stfu bc",
-	"Stfu and Gtfo U nub"
+    "you noob",
+    "Relax your Rear,ders nothing to fear,The Rape train is finally here",
+    "Stfu bc",
+    "Stfu and Gtfo U nub",
     "CUnt",
-    " Gay is here",
-"Owww ... Such a stupid idiot.",
+    "Gay is here",
+    "Owww ... Such a stupid idiot.",
     "Don't drink and type.",
     "I think you should go home or better a mental asylum.",
     "Command not found. Just like your brain.",
@@ -84,178 +84,6 @@ ABUSE_STRINGS = (
     "You can be the first person to step on sun. Have a try.",
 )
 
-EYES = [
-    ['⌐■', '■'],
-    [' ͠°', ' °'],
-    ['⇀', '↼'],
-    ['´• ', ' •`'],
-    ['´', '`'],
-    ['`', '´'],
-    ['ó', 'ò'],
-    ['ò', 'ó'],
-    ['⸌', '⸍'],
-    ['>', '<'],
-    ['Ƹ̵̡', 'Ʒ'],
-    ['ᗒ', 'ᗕ'],
-    ['⟃', '⟄'],
-    ['⪧', '⪦'],
-    ['⪦', '⪧'],
-    ['⪩', '⪨'],
-    ['⪨', '⪩'],
-    ['⪰', '⪯'],
-    ['⫑', '⫒'],
-    ['⨴', '⨵'],
-    ['⩿', '⪀'],
-    ['⩾', '⩽'],
-    ['⩺', '⩹'],
-    ['⩹', '⩺'],
-    ['◥▶', '◀◤'],
-    ['◍', '◎'],
-    ['/͠-', '┐͡-\\'],
-    ['⌣', '⌣”'],
-    [' ͡⎚', ' ͡⎚'],
-    ['≋'],
-    ['૦ઁ'],
-    ['  ͯ'],
-    ['  ͌'],
-    ['ළ'],
-    ['◉'],
-    ['☉'],
-    ['・'],
-    ['▰'],
-    ['ᵔ'],
-    [' ﾟ'],
-    ['□'],
-    ['☼'],
-    ['*'],
-    ['`'],
-    ['⚆'],
-    ['⊜'],
-    ['>'],
-    ['❍'],
-    ['￣'],
-    ['─'],
-    ['✿'],
-    ['•'],
-    ['T'],
-    ['^'],
-    ['ⱺ'],
-    ['@'],
-    ['ȍ'],
-    ['  '],
-    ['  '],
-    ['x'],
-    ['-'],
-    ['$'],
-    ['Ȍ'],
-    ['ʘ'],
-    ['Ꝋ'],
-    [''],
-    ['⸟'],
-    ['๏'],
-    ['ⴲ'],
-    ['◕'],
-    ['◔'],
-    ['✧'],
-    ['■'],
-    ['♥'],
-    [' ͡°'],
-    ['¬'],
-    [' º '],
-    ['⨶'],
-    ['⨱'],
-    ['⏓'],
-    ['⏒'],
-    ['⍜'],
-    ['⍤'],
-    ['ᚖ'],
-    ['ᴗ'],
-    ['ಠ'],
-    ['σ'],
-    ['☯']
-]
-
-MOUTHS = [
-    ['v'],
-    ['ᴥ'],
-    ['ᗝ'],
-    ['Ѡ'],
-    ['ᗜ'],
-    ['Ꮂ'],
-    ['ᨓ'],
-    ['ᨎ'],
-    ['ヮ'],
-    ['╭͜ʖ╮'],
-    [' ͟ل͜'],
-    [' ͜ʖ'],
-    [' ͟ʖ'],
-    [' ʖ̯'],
-    ['ω'],
-    [' ³'],
-    [' ε '],
-    ['﹏'],
-    ['□'],
-    ['ل͜'],
-    ['‿'],
-    ['╭╮'],
-    ['‿‿'],
-    ['▾'],
-    ['‸'],
-    ['Д'],
-    ['∀'],
-    ['!'],
-    ['人'],
-    ['.'],
-    ['ロ'],
-    ['_'],
-    ['෴'],
-    ['ѽ'],
-    ['ഌ'],
-    ['⏠'],
-    ['⏏'],
-    ['⍊'],
-    ['⍘'],
-    ['ツ'],
-    ['益'],
-    ['╭∩╮'],
-    ['Ĺ̯'],
-    ['◡'],
-    [' ͜つ']
-]
-
-EARS = [
-    ['q', 'p'],
-    ['ʢ', 'ʡ'],
-    ['⸮', '?'],
-    ['ʕ', 'ʔ'],
-    ['ᖗ', 'ᖘ'],
-    ['ᕦ', 'ᕥ'],
-    ['ᕦ(', ')ᕥ'],
-    ['ᕙ(', ')ᕗ'],
-    ['ᘳ', 'ᘰ'],
-    ['ᕮ', 'ᕭ'],
-    ['ᕳ', 'ᕲ'],
-    ['(', ')'],
-    ['[', ']'],
-    ['¯\\_', '_/¯'],
-    ['୧', '୨'],
-    ['୨', '୧'],
-    ['⤜(', ')⤏'],
-    ['☞', '☞'],
-    ['ᑫ', 'ᑷ'],
-    ['ᑴ', 'ᑷ'],
-    ['ヽ(', ')ﾉ'],
-    ['\\(', ')/'],
-    ['乁(', ')ㄏ'],
-    ['└[', ']┘'],
-    ['(づ', ')づ'],
-    ['(ง', ')ง'],
-    ['⎝', '⎠'],
-    ['ლ(', 'ლ)'],
-    ['ᕕ(', ')ᕗ'],
-    ['(∩', ')⊃━☆ﾟ.*'],
-]
-
 @run_async
 def abuse(bot: Bot, update: Update):
     # reply to correct message
@@ -269,14 +97,6 @@ def shrug(bot: Bot, update: Update):
     reply_text("¯\_(ツ)_/¯")	
 
 @run_async
-def rlg(bot: Bot, update: Update):
-    # reply to correct message
-    eyes = random.choice(EYES)
-    mouth = random.choice(MOUTHS)
-    ears = random.choice(EARS)
-    repl = format(ears + eyes + mouth + eyes + ears)
-    update.message.reply_text(repl)
-	
 def decide(bot: Bot, update: Update):
         r = randint(1, 100)
         if r <= 65:
@@ -300,6 +120,7 @@ def snipe(bot: Bot, update: Update, args: List[str]):
         except TelegramError:           
             LOGGER.warning("Couldn't send to group %s", str(chat_id))             
             update.effective_message.reply_text("Couldn't send the message. Perhaps I'm not part of that group?")  
+
 	
 __help__ = """
 Some random memes to make ur day!
@@ -307,18 +128,15 @@ Some random memes to make ur day!
  - /shrug : get shrug  ¯\_(ツ)_/¯
  - /decide : Randomly answers yes/no/maybe
  - /abuse : Abuses the retard!
- - /rlg : create an emoji ^_^
 """
 
 __mod_name__ = "Extras"
 SHRUG_HANDLER = DisableAbleCommandHandler("shrug", shrug)
-RLG_HANDLER = DisableAbleCommandHandler("rlg", rlg)
 DECIDE_HANDLER = DisableAbleCommandHandler("decide", decide)
 SNIPE_HANDLER = CommandHandler("snipe", snipe, pass_args=True, filters=CustomFilters.sudo_filter)
 ABUSE_HANDLER = DisableAbleCommandHandler("abuse", abuse)
 
 dispatcher.add_handler(SHRUG_HANDLER)
-dispatcher.add_handler(RLG_HANDLER)
 dispatcher.add_handler(DECIDE_HANDLER)
 dispatcher.add_handler(ABUSE_HANDLER)
 dispatcher.add_handler(SNIPE_HANDLER)
