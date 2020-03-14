3cec1171ad6530ed20f148a073b820db34426612
starry69 <starry369126@outlook.com>
2020-03-14 20:41:28 +0530

    Add logger start msg
    Signed-off-by: starry69 <starry369126@outlook.com>

--- skylee/__init__.py
+++ skylee/__init__.py
@@ -11,6 +11,8 @@ logging.basicConfig(
 
 LOGGER = logging.getLogger(__name__)
 
+LOGGER.info("Starting Skylee...")
+
 # if version < 3.6, stop bot.
 if sys.version_info[0] < 3 or sys.version_info[1] < 6:
     LOGGER.error("You MUST have a python version of at least 3.6! Multiple features depend on this. Bot quitting.")
