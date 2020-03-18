9d0e60c35b99e5a6938bbff62a6f2eee18bb15e9
starry69 <starry369126@outlook.com>
2020-03-18 15:00:56 +0530

    keyboard: remove donate button
    Signed-off-by: starry69 <starry369126@outlook.com>

--- skylee/modules/keyboard.py
+++ skylee/modules/keyboard.py
@@ -53,12 +53,11 @@ def keyboard(bot, update):
     update.effective_message.reply_text("keyboard updated",
                                             reply_markup=ReplyKeyboardMarkup([[
                                                 KeyboardButton("/help - Bot Help"), 
-                                                KeyboardButton("/donate - Donate"),
                                                 KeyboardButton("/notes - Notes")],
-                                             [KeyboardButton(btn1)], 
+                                             [KeyboardButton(btn1)],
                                              [KeyboardButton(btn2)],
                                              [KeyboardButton(btn3)]]))
-    
+
 
 KEYBOARD_HANDLER = CommandHandler(["keyboard"], keyboard)
 dispatcher.add_handler(KEYBOARD_HANDLER)
