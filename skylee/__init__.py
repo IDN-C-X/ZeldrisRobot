5d8968a052715101c33cd41a22b3d12abf3c7509
starry69 <starry369126@outlook.com>
2020-04-06 23:58:24 +0530

    Ignore if no sw api
    Signed-off-by: starry69 <starry369126@outlook.com>

--- skylee/__init__.py
+++ skylee/__init__.py
@@ -129,8 +129,11 @@ SUDO_USERS.add(OWNER_ID)
 # SpamWatch
 
 sw_token = Config.SPAMWATCH_API
-spamwtc = spamwatch.Client(sw_token)
-
+if sw_token == None:
+    spamwtc = None
+    LOGGER.warning("Invalid spamwatch api")
+else:
+    spamwtc = spamwatch.Client(sw_token)
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
 
 dispatcher = updater.dispatcher
