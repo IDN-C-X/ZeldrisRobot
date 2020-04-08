6219b5f52a132dccbf4d7de0221d83876c29e3d7
starry69 <starry369126@outlook.com>
2020-04-08 21:57:49 +0530

    Fix permission mess of muting
    Signed-off-by: starry69 <starry369126@outlook.com>

--- skylee/modules/muting.py
+++ skylee/modules/muting.py
@@ -92,10 +92,13 @@ def unmute(update, context):
             context.bot.restrict_chat_member(chat.id, int(user_id),
                                         permissions=ChatPermissions(
                                          can_send_messages=True,
-                                         can_send_media_messages=True,
-                                         can_send_other_messages=True,
-                                         can_add_web_page_previews=True)
-                                        )
+                                         can_invite_users=True,
+                                         can_pin_messages=True,
+                                         can_send_polls=True,
+                                         can_change_info=True,
+                                         can_send_media_messages=True, 
+                                         can_send_other_messages=True, 
+                                         can_add_web_page_previews=True))
             message.reply_text("Yep! this user can start talking again...")
             return "<b>{}:</b>" \
                    "\n#UNMUTE" \
