a1a565a83280e6d61d215e4a2bafc0c94afcb071
starry69 <starry369126@outlook.com>
2020-04-20 13:46:27 +0530

    Nuke keyboard module
    No longer needed with new connections since its using buttons instead
    
    Signed-off-by: starry69 <starry369126@outlook.com>

--- skylee/sample_config.py
+++ skylee/sample_config.py
@@ -18,7 +18,7 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
     MESSAGE_DUMP = None  # needed to make sure 'save from' messages persist
     LOAD = []
-    NO_LOAD = ['stickers']
+    NO_LOAD = []
     WEBHOOK = False
     URL = None
 
