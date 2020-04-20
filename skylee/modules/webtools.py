cbbd324f7dd7e1f53a384b0a95acbfe1ad3ff704
starry69 <starry369126@outlook.com>
2020-04-20 10:39:56 +0530

    webtools: sysinfo: switch to HTML parser to fix shitty markdown issues
    Signed-off-by: starry69 <starry369126@outlook.com>

--- skylee/modules/webtools.py
+++ skylee/modules/webtools.py
@@ -71,27 +71,27 @@ def speedtst(update, context):
 @run_async
 def system_status(update, context):
     uptime = datetime.datetime.fromtimestamp(boot_time()).strftime("%Y-%m-%d %H:%M:%S")
-    status = "*=======[ SYSTEM INFO ]=======*\n\n"
-    status += "*System uptime:* "+str(uptime)+"\n"
+    status = "<b>=======[ SYSTEM INFO ]=======</b>\n\n"
+    status += "<b>System uptime:</b> <code>"+str(uptime)+"</code>\n"
 
     uname = platform.uname()
-    status += "*System:* "+str(uname.system)+"\n"
-    status += "*Node name:* "+str(uname.node)+"\n"
-    status += "*Release:* "+str(uname.release)+"\n"
-    status += "*Version:* "+str(uname.version)+"\n"
-    status += "*Machine:* "+str(uname.machine)+"\n"
-    status += "*Processor:* "+str(uname.processor)+"\n\n"
+    status += "<b>System:</b> <code>"+str(uname.system)+"</code>\n"
+    status += "<b>Node name:</b> <code>"+str(uname.node)+"</code>\n"
+    status += "<b>Release:</b> <code>"+str(uname.release)+"</code>\n"
+    status += "<b>Version:</b> <code>"+str(uname.version)+"</code>\n"
+    status += "<b>Machine:</b> <code>"+str(uname.machine)+"</code>\n"
+    status += "<b>Processor:</b> <code>"+str(uname.processor)+"</code>\n\n"
 
     mem = virtual_memory()
     cpu = cpu_percent()
     disk = disk_usage('/')
-    status += "*CPU usage:* "+str(cpu)+" %\n"
-    status += "*Ram usage:* "+str(mem[2])+" %\n"
-    status += "*Storage used:* "+str(disk[3])+" %\n\n"
-    status += "*Python version:* "+python_version()+"\n"
-    status += "*Library version:* "+str(__version__)+"\n"
-    status += "*Spamwatch API:* "+str(__sw__)+"\n"
-    update.effective_message.reply_text(status, parse_mode=ParseMode.MARKDOWN)
+    status += "<b>CPU usage:</b> <code>"+str(cpu)+" %</code>\n"
+    status += "<b>Ram usage:</b> <code>"+str(mem[2])+" %</code>\n"
+    status += "<b>Storage used:</b> <code>"+str(disk[3])+" %</code>\n\n"
+    status += "<b>Python version:</b> <code>"+python_version()+"</code>\n"
+    status += "<b>Library version:</b> <code>"+str(__version__)+"</code>\n"
+    status += "<b>Spamwatch API:</b> <code>"+str(__sw__)+"</code>\n"
+    update.effective_message.reply_text(status, parse_mode=ParseMode.HTML)
 
 
 IP_HANDLER = CommandHandler("ip", get_bot_ip, filters=Filters.chat(OWNER_ID))
