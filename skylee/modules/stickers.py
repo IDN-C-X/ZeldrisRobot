aff5170d5acfce54e9e8d9ad853dc24b6ff45e74
starry69 <starry369126@outlook.com>
2020-04-25 13:17:26 +0530

    Stickers: fix derp
    Signed-off-by: starry69 <starry369126@outlook.com>

--- skylee/modules/stickers.py
+++ skylee/modules/stickers.py
@@ -191,7 +191,7 @@ def makepack_internal(update, context, msg, user, png_sticker, emoji, packname, 
                            parse_mode=ParseMode.MARKDOWN)
         elif e.message == "Peer_id_invalid":
             msg.reply_text("Contact me in PM first.", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(
-                text="Start", url=f"t.me/{bot.username}")]]))
+                text="Start", url=f"t.me/{context.bot.username}")]]))
         elif e.message == "Internal Server Error: created sticker set not found (500)":
                 msg.reply_text("Sticker pack successfully created. Get it [here](t.me/addstickers/%s)" % packname,
                        parse_mode=ParseMode.MARKDOWN)
