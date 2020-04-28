9381ab24b3f661189e5f040b806d199c198ffcb3
starry69 <starry369126@outlook.com>
2020-04-28 19:53:34 +0530

    Afk: Add randomised reply_afk strings.
    Afk strings credits: PaperPlaneExtended userbot
    
    Signed-off-by: starry69 <starry369126@outlook.com>

--- skylee/modules/afk.py
+++ skylee/modules/afk.py
@@ -1,7 +1,8 @@
 from typing import Optional
+import random
 
 from telegram import Message, Update, Bot, User
-from telegram import MessageEntity
+from telegram import MessageEntity, ParseMode
 from telegram.error import BadRequest
 from telegram.ext import Filters, MessageHandler, run_async
 
@@ -11,6 +12,7 @@ from skylee.modules.sql import afk_sql as sql
 from skylee.modules.users import get_user_id
 
 from skylee.modules.helper_funcs.alternate import send_message
+import skylee.modules.helper_funcs.fun_strings as fun
 
 AFK_GROUP = 7
 AFK_REPLY_GROUP = 8
@@ -25,7 +27,7 @@ def afk(update, context):
         reason = ""
 
     sql.set_afk(update.effective_user.id, reason)
-    send_message(update.effective_message, "Cya 👋 {}!".format(update.effective_user.first_name))
+    send_message(update.effective_message, f"Cya 👋 {update.effective_user.first_name}!")
 
 
 @run_async
@@ -37,7 +39,7 @@ def no_longer_afk(update, context):
 
     res = sql.rm_afk(user.id)
     if res:
-        send_message(update.effective_message, "Welcome back {}!".format(update.effective_user.first_name))
+        send_message(update.effective_message, f"Welcome back {user.first_name}!")
 
 
 @run_async
@@ -70,10 +72,11 @@ def reply_afk(update, context):
                 valid, reason = sql.check_afk_status(user_id)
                 if valid:
                     if not reason:
-                        res = "Hang on... {} away from keyboard!".format(fst_name)
+                        afkstr = random.choice(fun.AFK)
+                        res = afkstr.format(fst_name)
                     else:
-                        res = "{} is away from keyboard! says it's because of: \n{}".format(fst_name, reason)
-                    send_message(update.effective_message, res)
+                        res = f"<b>{fst_name}</b> is away from keyboard! says it's because of \n{reason}"
+                    send_message(update.effective_message, res, parse_mode=ParseMode.HTML)
 
 
 def __user_info__(user_id):
--- skylee/modules/helper_funcs/fun_strings.py
+++ skylee/modules/helper_funcs/fun_strings.py
@@ -319,6 +319,7 @@ GBUN = (
     "What if there was 'a' instead of 'u' ...👀",
     "I guess you've forgot spelling of gban maybe...?",
     "OK no!",
+    "OK boomer.",
     "Don't misuse your powers noob...",
     "Nah, he looks innocent...",
     "Get ready for global bun hammer!"
@@ -329,3 +330,19 @@ DECIDE = (
     "NoU.",
     "Maybe."
 )
+
+# AFK strings credits: PaperPlaneExtended!
+AFK = (
+    "<b>{}</b> is busy right now. Please talk in a bag and when they come back you can just give him the bag!",
+    "<b>{}</b> is away right now. If you need anything, leave a message after the beep:\n<code>beeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeep</code>!",
+    "You just missed <b>{}</b>, next time aim better.",
+    "{} will be back in a few minutes and if not...,\nwait longer.",
+    "Roses are red,\nViolets are blue,\nLeave me a message,\nAnd {} will get back to you.",
+    "If you haven't figured it out already,\n{} is not here.",
+    "{} went that way\n---->",
+    "{} went went this way\n<----",
+    "{} is not here so stop writing to them,\nor else you will find yourself with a screen full of your own messages.",
+    "Life is so short, there are so many things to do...\n{} is away doing one of them..",
+    "{} is away from the keyboard at the moment, but if you'll scream loud enough at your screen, they might just hear you."
+)
+
