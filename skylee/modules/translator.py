77f8bee77cd69ab8510b2e69e936e29952562989
Paul Larsen <pnvlarsen@gmail.com>
2020-05-25 23:34:51 +0530

    Translator: Add grammer checker function
    Signed-off-by: starry69 <starry369126@outlook.com>

--- skylee/modules/translator.py
+++ skylee/modules/translator.py
@@ -2,6 +2,8 @@ from typing import Optional, List
 from gtts import gTTS
 import re
 import os
+import requests
+import json
 from emoji import UNICODE_EMOJI
 
 from telegram import Message, Update, Bot, User, ChatAction, MessageEntity, ParseMode
@@ -22,7 +24,7 @@ def gtrans(update, context):
     lang = " ".join(args)
     if not lang:
        lang = "en"
-    translate_text = msg.reply_to_message.text
+    translate_text = msg.reply_to_message
     ignore_text = UNICODE_EMOJI.keys()
     for emoji in ignore_text:
         if emoji in translate_text:
@@ -62,12 +64,47 @@ def gtts(update, context):
             update.effective_message.reply_text("Reply to some message or enter some text to convert it into audio format!")
 
 
-__help__ = """
-× /tr - To translate to your language, by default language is set to english, use `/tr <lang code>` for some other language!
+# Open API key
+API_KEY = "6ae0c3a0-afdc-4532-a810-82ded0054236"
+URL = "http://services.gingersoftware.com/Ginger/correct/json/GingerTheText"
 
-× /tts - To some message to convert it into audio format! 
+@run_async
+@typing_action
+def spellcheck(update, context):
+    if update.effective_message.reply_to_message:
+        msg = update.effective_message.reply_to_message
+
+        params = dict(lang="US", clientVersion="2.0",
+                 apiKey=API_KEY, text=msg.text)
+
+        res = requests.get(URL, params=params)
+        changes = json.loads(res.text).get('LightGingerTheTextResult')
+        curr_string = ""
+        prev_end = 0
+
+        for change in changes:
+            start = change.get('From')
+            end = change.get('To') + 1
+            suggestions = change.get('Suggestions')
+            if suggestions:
+                sugg_str = suggestions[0].get('Text')  # should look at this list more
+                curr_string += msg.text[prev_end:start] + sugg_str
+                prev_end = end
+
+        curr_string += msg.text[prev_end:]
+        update.effective_message.reply_text(curr_string)
+    else:
+       update.effective_message.reply_text(
+              "Reply to some message to get grammar corrected text!")
+
+
+__help__ = """
+× /tr or /tl: - To translate to your language, by default language is set to english, use `/tr <lang code>` for some other language!
+× /splcheck: - As a reply to get grammar corrected text of gibberish message.
+× /tts: - To some message to convert it into audio format!
 """
 __mod_name__ = "Translate"
 
 dispatcher.add_handler(DisableAbleCommandHandler(["tr", "tl"], gtrans, pass_args=True))
 dispatcher.add_handler(DisableAbleCommandHandler("tts", gtts, pass_args=True))
+dispatcher.add_handler(DisableAbleCommandHandler("splcheck", spellcheck))
