490ddd8e939936b38cc6ab05a4f63e58f6b0c6e3
starry69 <starry369126@outlook.com>
2020-06-01 10:35:28 +0530

    Purge: Address exception
    Signed-off-by: starry69 <starry369126@outlook.com>

--- skylee/modules/purge.py
+++ skylee/modules/purge.py
@@ -3,6 +3,7 @@ from skylee import client, SUDO_USERS
 import asyncio
 from telethon import events
 from telethon.tl.types import ChannelParticipantsAdmins
+from telethon.errors.rpcerrorlist import MessageDeleteForbiddenError
 
 # Check if user has admin rights
 async def is_administrator(user_id: int, message):
@@ -29,22 +30,31 @@ async def purge(event):
            await event.reply("Reply to a message to select where to start purging from.")
            return
 
-        msg_id = msg.id
-        to_delete = event.message.id - 1
-        await event.client.delete_messages(chat, event.message.id)
-        msgs.append(event.reply_to_msg_id)
-        for m_id in range(to_delete, msg_id - 1, -1):
-            msgs.append(m_id)
-            if len(msgs) == 100:
-               await event.client.delete_messages(chat, msgs)
-               msgs = []
+        try:
+           msg_id = msg.id
+           to_delete = event.message.id - 1
+           await event.client.delete_messages(chat, event.message.id)
+           msgs.append(event.reply_to_msg_id)
+           for m_id in range(to_delete, msg_id - 1, -1):
+               msgs.append(m_id)
+               if len(msgs) == 100:
+                   await event.client.delete_messages(chat, msgs)
+                   msgs = []
 
-        await event.client.delete_messages(chat, msgs)
-        del_res = await event.client.send_message(
-        event.chat_id, "Flash purge complete!")
+           await event.client.delete_messages(chat, msgs)
+           del_res = await event.client.send_message(
+           event.chat_id, "Flash purge complete!")
 
-        await asyncio.sleep(4)
-        await del_res.delete()
+           await asyncio.sleep(4)
+           await del_res.delete()
+
+        except MessageDeleteForbiddenError:
+            text = "Failed to delete messages.\n"
+            text += "Messages maybe too old or I'm not admin!"
+            del_res = await event.respond(text, parse_mode='md')
+            await asyncio.sleep(5)
+            await del_res.delete()
+
 
 @client.on(events.NewMessage(pattern="^/del$"))
 async def delete_msg(event):
