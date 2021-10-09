d426aa12840ab6f939c5d768c7594d5fca9952fd
ZERO • アクバル <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-10-09 20:36:34 +0700

    zeldris: improve alternate

--- zeldris/modules/helper_funcs/alternate.py
+++ zeldris/modules/helper_funcs/alternate.py
@@ -2,6 +2,8 @@ from functools import wraps
 
 from telegram import error, ChatAction
 
+from zeldris import LOGGER
+
 
 def send_message(message, text, *args, **kwargs):
     try:
@@ -9,6 +11,7 @@ def send_message(message, text, *args, **kwargs):
     except error.BadRequest as err:
         if str(err) == "Reply message not found":
             return message.reply_text(text, quote=False, *args, **kwargs)
+        raise
 
 
 def typing_action(func):
@@ -16,10 +19,20 @@ def typing_action(func):
 
     @wraps(func)
     def command_func(update, context, *args, **kwargs):
-        context.bot.send_chat_action(
-            chat_id=update.effective_chat.id, action=ChatAction.TYPING
-        )
-        return func(update, context, *args, **kwargs)
+        try:
+            context.bot.send_chat_action(
+                chat_id=update.effective_chat.id, action=ChatAction.TYPING
+            )
+            return func(update, context, *args, **kwargs)
+        except error.BadRequest as err:
+            if str(err) == "Have no rights to send a message":
+                LOGGER.warning("Bot muted in {} {}".format(
+                        update.effective_message.chat.title,
+                        update.effective_message.chat.id
+                    )
+                )
+        except error.Unauthorized:
+            return
 
     return command_func
 
