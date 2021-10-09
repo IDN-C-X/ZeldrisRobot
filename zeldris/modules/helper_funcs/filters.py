94d942653c0a5aeab8eac0c4d99f7cd4ecd3b048
ZERO • アクバル <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-10-10 04:24:30 +0700

    Update filters.py

--- zeldris/modules/helper_funcs/filters.py
+++ zeldris/modules/helper_funcs/filters.py
@@ -1,7 +1,7 @@
 from telegram import Message
 from telegram.ext import MessageFilter
 
-from zeldris import SUPPORT_USERS, SUDO_USERS
+from zeldris import SUPPORT_USERS, SUDO_USERS, DEV_USERS
 
 
 class CustomFilters(object):
@@ -16,6 +16,12 @@ class CustomFilters(object):
             return bool(message.from_user and message.from_user.id in SUDO_USERS)
 
     sudo_filter = _Sudoers()
+
+    class _Developers(MessageFilter):
+        def filter(self, message: Message):
+            return bool(message.from_user and message.from_user.id in DEV_USERS)
+
+    dev_filter = _Developers()
 
     class _MimeType(MessageFilter):
         def __init__(self, mimetype):
