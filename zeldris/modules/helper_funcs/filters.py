8fb9fb0ed852ad13b80a6d8b75a60734494be4bf
ZERO • アクバル <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-10-10 04:44:47 +0700

    zeldris: revert

--- zeldris/modules/helper_funcs/filters.py
+++ zeldris/modules/helper_funcs/filters.py
@@ -1,29 +1,29 @@
 from telegram import Message
-from telegram.ext import MessageFilter
+from telegram.ext import BaseFilter
 
 from zeldris import SUPPORT_USERS, SUDO_USERS, DEV_USERS
 
 
 class CustomFilters(object):
-    class _Supporters(MessageFilter):
+    class _Supporters(BaseFilter):
         def filter(self, message: Message):
             return bool(message.from_user and message.from_user.id in SUPPORT_USERS)
 
     support_filter = _Supporters()
 
-    class _Sudoers(MessageFilter):
+    class _Sudoers(BaseFilter):
         def filter(self, message: Message):
             return bool(message.from_user and message.from_user.id in SUDO_USERS)
 
     sudo_filter = _Sudoers()
 
-    class _Developers(MessageFilter):
+    class _Developers(BaseFilter):
         def filter(self, message: Message):
             return bool(message.from_user and message.from_user.id in DEV_USERS)
 
     dev_filter = _Developers()
 
-    class _MimeType(MessageFilter):
+    class _MimeType(BaseFilter):
         def __init__(self, mimetype):
             self.mime_type = mimetype
             self.name = "CustomFilters.mime_type({})".format(self.mime_type)
@@ -35,7 +35,7 @@ class CustomFilters(object):
 
     mime_type = _MimeType
 
-    class _HasText(MessageFilter):
+    class _HasText(BaseFilter):
         def filter(self, message: Message):
             return bool(
                 message.text
