462a6f0f933dc8309ae98849c4b94977d3122a33
ZERO • アクバル <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-10-10 16:45:35 +0700

    zeldris: new feature, added anime modules

---
+++ zeldris/modules/anime.py
@@ -0,0 +1,490 @@
+import datetime
+import textwrap
+
+import jikanpy
+import requests
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update, Message
+
+from zeldris import dispatcher
+from zeldris.modules.disable import DisableAbleCommandHandler
+
+info_btn = "More Information"
+prequel_btn = "⬅️ Prequel"
+sequel_btn = "Sequel ➡️"
+close_btn = "Close ❌"
+
+
+def shorten(description, info="anilist.co"):
+    msg = ""
+    if len(description) > 700:
+        description = description[0:500] + "...."
+        msg += f"\n*Description*: _{description}_[Read More]({info})"
+    else:
+        msg += f"\n*Description*:_{description}_"
+    return msg
+
+
+# time formatter from uniborg
+def t(milliseconds: int) -> str:
+    """Inputs time in milliseconds, to get beautified time,
+    as string"""
+    seconds, milliseconds = divmod(int(milliseconds), 1000)
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    tmp = (
+            ((str(days) + " Days, ") if days else "")
+            + ((str(hours) + " Hours, ") if hours else "")
+            + ((str(minutes) + " Minutes, ") if minutes else "")
+            + ((str(seconds) + " Seconds, ") if seconds else "")
+            + ((str(milliseconds) + " ms, ") if milliseconds else "")
+    )
+    return tmp[:-2]
+
+
+airing_query = """
+query ($id: Int,$search: String) {
+    Media (id: $id, type: ANIME,search: $search) {
+        id
+        episodes
+        title {
+            romaji
+            english
+            native
+        }
+        nextAiringEpisode {
+            airingAt
+            timeUntilAiring
+            episode
+        }
+    }
+}
+"""
+
+fav_query = """
+query ($id: Int) {
+    Media (id: $id, type: ANIME) {
+        id
+        title {
+            romaji
+            english
+            native
+        }
+    }
+}
+"""
+
+anime_query = """
+query ($id: Int,$search: String) {
+    Media (id: $id, type: ANIME,search: $search) {
+        id
+        title {
+            romaji
+            english
+            native
+        }
+        description (asHtml: false)
+        startDate{
+            year
+        }
+        episodes
+        season
+        type
+        format
+        status
+        duration
+        siteUrl
+        studios{
+            nodes{
+                name
+            }
+        }
+        trailer{
+            id
+            site
+            thumbnail
+        }
+        averageScore
+        genres
+        bannerImage
+    }
+}
+"""
+
+character_query = """
+query ($query: String) {
+    Character (search: $query) {
+        id
+        name {
+            first
+            last
+            full
+        }
+        siteUrl
+        image {
+            large
+        }
+        description
+    }
+}
+"""
+
+manga_query = """
+query ($id: Int,$search: String) {
+    Media (id: $id, type: MANGA,search: $search) {
+        id
+        title {
+            romaji
+            english
+            native
+        }
+        description (asHtml: false)
+        startDate{
+            year
+        }
+        type
+        format
+        status
+        siteUrl
+        averageScore
+        genres
+        bannerImage
+    }
+}
+"""
+
+url = "https://graphql.anilist.co"
+
+
+def extract_arg(message: Message):
+    split = message.text.split(" ", 1)
+    if len(split) > 1:
+        return split[1]
+    reply = message.reply_to_message
+    if reply is not None:
+        return reply.text
+    return None
+
+
+def airing(update: Update, _):
+    message = update.effective_message
+    search_str = extract_arg(message)
+    if not search_str:
+        update.effective_message.reply_text(
+            "Tell Anime Name :) ( /airing <anime name>)",
+        )
+        return
+    variables = {"search": search_str}
+    response = requests.post(
+        url, json={"query": airing_query, "variables": variables},
+    ).json()["data"]["Media"]
+    msg = f"*Name*: *{response['title']['romaji']}*(`{response['title']['native']}`)\n*ID*: `{response['id']}`"
+    if response["nextAiringEpisode"]:
+        time = response["nextAiringEpisode"]["timeUntilAiring"] * 1000
+        time = t(time)
+        msg += f"\n*Episode*: `{response['nextAiringEpisode']['episode']}`\n*Airing In*: `{time}`"
+    else:
+        msg += f"\n*Episode*:{response['episodes']}\n*Status*: `N/A`"
+    update.effective_message.reply_text(msg, parse_mode=ParseMode.MARKDOWN)
+
+
+def anime(update: Update, _):
+    message = update.effective_message
+    search = extract_arg(message)
+    if not search:
+        update.effective_message.reply_text("Format : /anime < anime name >")
+        return
+    variables = {"search": search}
+    json = requests.post(
+        url, json={"query": anime_query, "variables": variables},
+    ).json()
+    if "errors" in json.keys():
+        update.effective_message.reply_text("Anime not found")
+        return
+    if json:
+        json = json["data"]["Media"]
+        msg = f"*{json['title']['romaji']}*(`{json['title']['native']}`)\n" \
+              f"*Type*: {json['format']}\n*Status*: {json['status']}\n" \
+              f"*Episodes*: {json.get('episodes', 'N/A')}\n" \
+              f"*Duration*: {json.get('duration', 'N/A')} Per Ep.\n" \
+              f"*Score*: {json['averageScore']}\n" \
+              f"*Genres*: `"
+        for x in json["genres"]:
+            msg += f"{x}, "
+        msg = msg[:-2] + "`\n"
+        msg += "*Studios*: `"
+        for x in json["studios"]["nodes"]:
+            msg += f"{x['name']}, "
+        msg = msg[:-2] + "`\n"
+        info = json.get("siteUrl")
+        trailer = json.get("trailer", None)
+        anime_id = json["id"]
+        if trailer:
+            trailer_id = trailer.get("id", None)
+            site = trailer.get("site", None)
+            if site == "youtube":
+                trailer = "https://youtu.be/" + trailer_id
+        description = (
+            json.get("description", "N/A")
+                .replace("<i>", "")
+                .replace("</i>", "")
+                .replace("<br>", "")
+        )
+        msg += shorten(description, info)
+        image = json.get("bannerImage", None)
+        if trailer:
+            buttons = [
+                [
+                    InlineKeyboardButton("More Info", url=info),
+                    InlineKeyboardButton("Trailer 🎬", url=trailer),
+                ],
+            ]
+        else:
+            buttons = [[InlineKeyboardButton("More Info", url=info)]]
+        if image:
+            try:
+                update.effective_message.reply_photo(
+                    photo=image,
+                    caption=msg,
+                    parse_mode=ParseMode.MARKDOWN,
+                    reply_markup=InlineKeyboardMarkup(buttons),
+                )
+            except BaseException:
+                msg += f" [〽️]({image})"
+                update.effective_message.reply_text(
+                    msg,
+                    parse_mode=ParseMode.MARKDOWN,
+                    reply_markup=InlineKeyboardMarkup(buttons),
+                )
+        else:
+            update.effective_message.reply_text(
+                msg,
+                parse_mode=ParseMode.MARKDOWN,
+                reply_markup=InlineKeyboardMarkup(buttons),
+            )
+
+
+def character(update: Update, _):
+    message = update.effective_message
+    search = extract_arg(message)
+    if not search:
+        update.effective_message.reply_text("Format : /character < character name >")
+        return
+    variables = {"query": search}
+    json = requests.post(
+        url, json={"query": character_query, "variables": variables},
+    ).json()
+    if "errors" in json.keys():
+        update.effective_message.reply_text("Character not found")
+        return
+    if json:
+        json = json["data"]["Character"]
+        msg = f"*{json.get('name').get('full')}*(`{json.get('name').get('native')}`)\n"
+        description = f"{json['description']}"
+        site_url = json.get("siteUrl")
+        msg += shorten(description, site_url)
+        image = json.get("image", None)
+        if image:
+            image = image.get("large")
+            update.effective_message.reply_photo(
+                photo=image,
+                caption=msg.replace("<b>", "</b>"),
+                parse_mode=ParseMode.MARKDOWN,
+            )
+        else:
+            update.effective_message.reply_text(
+                msg.replace("<b>", "</b>"), parse_mode=ParseMode.MARKDOWN,
+            )
+
+
+def manga(update: Update, _):
+    message = update.effective_message
+    search = extract_arg(message)
+    if not search:
+        update.effective_message.reply_text("Format : /manga < manga name >")
+        return
+    variables = {"search": search}
+    json = requests.post(
+        url, json={"query": manga_query, "variables": variables},
+    ).json()
+    msg = ""
+    if "errors" in json.keys():
+        update.effective_message.reply_text("Manga not found")
+        return
+    if json:
+        json = json["data"]["Media"]
+        title, title_native = json["title"].get("romaji", False), json["title"].get(
+            "native", False,
+        )
+        start_date, status, score = (
+            json["startDate"].get("year", False),
+            json.get("status", False),
+            json.get("averageScore", False),
+        )
+        if title:
+            msg += f"*{title}*"
+            if title_native:
+                msg += f"(`{title_native}`)"
+        if start_date:
+            msg += f"\n*Start Date* - `{start_date}`"
+        if status:
+            msg += f"\n*Status* - `{status}`"
+        if score:
+            msg += f"\n*Score* - `{score}`"
+        msg += "\n*Genres* - "
+        for x in json.get("genres", []):
+            msg += f"{x}, "
+        msg = msg[:-2]
+        info = json["siteUrl"]
+        buttons = [[InlineKeyboardButton("More Info", url=info)]]
+        image = json.get("bannerImage", False)
+        msg += f"_{json.get('description', None)}_"
+        if image:
+            try:
+                update.effective_message.reply_photo(
+                    photo=image,
+                    caption=msg,
+                    parse_mode=ParseMode.MARKDOWN,
+                    reply_markup=InlineKeyboardMarkup(buttons),
+                )
+            except BaseException:
+                msg += f" [〽️]({image})"
+                update.effective_message.reply_text(
+                    msg,
+                    parse_mode=ParseMode.MARKDOWN,
+                    reply_markup=InlineKeyboardMarkup(buttons),
+                )
+        else:
+            update.effective_message.reply_text(
+                msg,
+                parse_mode=ParseMode.MARKDOWN,
+                reply_markup=InlineKeyboardMarkup(buttons),
+            )
+
+
+def user(update: Update, _):
+    message = update.effective_message
+    search_query = extract_arg(message)
+
+    if not search_query:
+        update.effective_message.reply_text("Format : /user <username>")
+        return
+
+    jikan = jikanpy.jikan.Jikan()
+
+    try:
+        us = jikan.user(search_query)
+    except jikanpy.APIException:
+        update.effective_message.reply_text("Username not found.")
+        return
+
+    progress_message = update.effective_message.reply_text("Searching.... ")
+
+    date_format = "%Y-%m-%d"
+    if us["image_url"] is None:
+        img = "https://cdn.myanimelist.net/images/questionmark_50.gif"
+    else:
+        img = us["image_url"]
+
+    try:
+        user_birthday = datetime.datetime.fromisoformat(us["birthday"])
+        user_birthday_formatted = user_birthday.strftime(date_format)
+    except BaseException:
+        user_birthday_formatted = "Unknown"
+
+    user_joined_date = datetime.datetime.fromisoformat(us["joined"])
+    user_joined_date_formatted = user_joined_date.strftime(date_format)
+
+    for entity in us:
+        if us[entity] is None:
+            us[entity] = "Unknown"
+
+    about = us["about"].split(" ", 60)
+
+    try:
+        about.pop(60)
+    except IndexError:
+        pass
+
+    about_string = " ".join(about)
+    about_string = about_string.replace("<br>", "").strip().replace("\r\n", "\n")
+
+    caption = ""
+
+    caption += textwrap.dedent(
+        f"""
+    *Username*: [{us['username']}]({us['url']})
+
+    *Gender*: `{us['gender']}`
+    *Birthday*: `{user_birthday_formatted}`
+    *Joined*: `{user_joined_date_formatted}`
+    *Days wasted watching anime*: `{us['anime_stats']['days_watched']}`
+    *Days wasted reading manga*: `{us['manga_stats']['days_read']}`
+
+    """,
+    )
+
+    caption += f"*About*: {about_string}"
+
+    buttons = [
+        [InlineKeyboardButton(info_btn, url=us["url"])],
+        [
+            InlineKeyboardButton(
+                close_btn, callback_data=f"anime_close, {message.from_user.id}",
+            ),
+        ],
+    ]
+
+    update.effective_message.reply_photo(
+        photo=img,
+        caption=caption,
+        parse_mode=ParseMode.MARKDOWN,
+        reply_markup=InlineKeyboardMarkup(buttons),
+    )
+    progress_message.delete()
+
+
+def upcoming(update: Update, _):
+    jikan = jikanpy.jikan.Jikan()
+    upcomin = jikan.top("anime", page=1, subtype="upcoming")
+
+    upcoming_list = [entry["title"] for entry in upcomin["top"]]
+    upcoming_message = ""
+
+    for entry_num in range(len(upcoming_list)):
+        if entry_num == 10:
+            break
+        upcoming_message += f"{entry_num + 1}. {upcoming_list[entry_num]}\n"
+
+    update.effective_message.reply_text(upcoming_message)
+
+
+__help__ = """
+Get information about anime, manga or characters from [AniList](anilist.co).
+
+*Available commands:*
+
+× /anime <anime>: returns information about the anime.
+× /character <character>: returns information about the character.
+× /manga <manga>: returns information about the manga.
+× /user <user>`*:* returns information about a MyAnimeList user.
+× /upcoming: returns a list of new anime in the upcoming seasons.
+× /airing <anime>: returns anime airing info.
+
+ """
+
+ANIME_HANDLER = DisableAbleCommandHandler("anime", anime, run_async=True)
+AIRING_HANDLER = DisableAbleCommandHandler("airing", airing, run_async=True)
+CHARACTER_HANDLER = DisableAbleCommandHandler("character", character, run_async=True)
+MANGA_HANDLER = DisableAbleCommandHandler("manga", manga, run_async=True)
+USER_HANDLER = DisableAbleCommandHandler("user", user, run_async=True)
+UPCOMING_HANDLER = DisableAbleCommandHandler("upcoming", upcoming, run_async=True)
+
+dispatcher.add_handler(ANIME_HANDLER)
+dispatcher.add_handler(CHARACTER_HANDLER)
+dispatcher.add_handler(MANGA_HANDLER)
+dispatcher.add_handler(AIRING_HANDLER)
+dispatcher.add_handler(USER_HANDLER)
+dispatcher.add_handler(UPCOMING_HANDLER)
+
+__mod_name__ = "Anime"
