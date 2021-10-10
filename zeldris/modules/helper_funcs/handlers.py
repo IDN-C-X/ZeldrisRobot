acba10793af14b06c2982b7eff27f79f200d0d40
Ihsan <78595134+UserLazy@users.noreply.github.com>
2021-10-10 18:19:34 +0700

    Update handlers.py

--- zeldris/modules/helper_funcs/handlers.py
+++ zeldris/modules/helper_funcs/handlers.py
@@ -1,7 +1,7 @@
 import telegram.ext as tg
 from telegram import Update
 
-from zeldris import LOGGER
+from zeldris import LOGGER, DEV_USERS, SUDO_USERS, SUPPORT_USERS, WHITELIST_USERS
 
 try:
     from zeldris import CUSTOM_CMD
@@ -13,6 +13,45 @@ if CUSTOM_CMD:
     LOGGER.debug("Bot custom command handler = \"%s\"", CMD_STARTERS)
 else:
     CMD_STARTERS = ["/", "!']
+                    
+                    
+class AntiSpam:
+    def __init__(self):
+        self.whitelist = (
+            (DEV_USERS or [])
+            + (SUDO_USERS or [])
+            + (WHITELIST_USERS or [])
+            + (SUPPORT_USERS or [])
+        )
+        # Values are HIGHLY experimental, its recommended you pay attention to our commits as we will be adjusting the values over time with what suits best.
+        Duration.CUSTOM = 15  # Custom duration, 15 seconds
+        self.sec_limit = RequestRate(6, Duration.CUSTOM)  # 6 / Per 15 Seconds
+        self.min_limit = RequestRate(20, Duration.MINUTE)  # 20 / Per minute
+        self.hour_limit = RequestRate(100, Duration.HOUR)  # 100 / Per hour
+        self.daily_limit = RequestRate(1000, Duration.DAY)  # 1000 / Per day
+        self.limiter = Limiter(
+            self.sec_limit,
+            self.min_limit,
+            self.hour_limit,
+            self.daily_limit,
+            bucket_class=MemoryListBucket,
+        )
+
+    def check_user(self, user):
+        """
+        Return True if user is to be ignored else False
+        """
+        if user in self.whitelist:
+            return False
+        try:
+            self.limiter.try_acquire(user)
+            return False
+        except BucketFullException:
+            return True
+
+
+SpamChecker = AntiSpam()
+MessageHandlerChecker = AntiSpam()
 
 
 class CustomCommandHandler(tg.CommandHandler):
@@ -41,7 +80,8 @@ class CustomCommandHandler(tg.CommandHandler):
                         and command[1].lower() == message.bot.username.lower()
                     ):
                         return None
-
+                    if SpamChecker.check_user(user_id):
+                    return None
                     filter_result = self.filters(update)
                     if filter_result:
                         return args, filter_result
