02408b38f30309cfe6fb0bc1798f409fa07fc2cc
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-10-10 08:35:09 +0700

    Update regex.py

--- zeldris/modules/regex.py
+++ zeldris/modules/regex.py
@@ -117,7 +117,7 @@ def sed(update, context):
 
 
 SED_HANDLER = DisableAbleMessageHandler(
-    Filters.regex(r"s([{}]).*?\1.*".format("".join(DELIMITERS))), sed, friendly="sed", run_async=True
+    Filters.regex(r"s([{}]).*?\1.*".format("".join(DELIMITERS))), sed, friendly="sed"
 )
 
 dispatcher.add_handler(SED_HANDLER)
