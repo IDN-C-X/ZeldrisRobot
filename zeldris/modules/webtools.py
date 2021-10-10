48299e7dc60a573dc5093f939fd14bc39cff36e8
ZERO • アクバル <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-10-10 16:18:28 +0700

    the hell

--- zeldris/modules/webtools.py
+++ zeldris/modules/webtools.py
@@ -8,7 +8,7 @@ import speedtest
 from psutil import cpu_percent, virtual_memory, disk_usage, boot_time
 from pythonping import ping as ping3
 from spamwatch import __version__ as __sw__
-from telegram import ParseMode
+from telegram import ParseMode, Update
 from telegram import __version__
 from telegram.ext import CallbackContext, CommandHandler, Filters
 
