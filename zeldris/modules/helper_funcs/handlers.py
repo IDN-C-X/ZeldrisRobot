0f73284ce31a18d8b2aacbd83e6220f56a419c81
Ihsan <78595134+UserLazy@users.noreply.github.com>
2021-10-10 18:11:49 +0700

    tes

--- zeldris/modules/helper_funcs/handlers.py
+++ zeldris/modules/helper_funcs/handlers.py
@@ -1,118 +1,49 @@
-from telegram.ext import CommandHandler
-
-from pyrate_limiter import (
-    BucketFullException,
-    Duration,
-    RequestRate,
-    Limiter,
-    MemoryListBucket,
-)
+import telegram.ext as tg
 from telegram import Update
 
-from zeldris import DEV_USERS, SUDO_USERS, SUPPORT_USERS, WHITELIST_USERS
+from zeldris import LOGGER
 
 try:
     from zeldris import CUSTOM_CMD
-except:
+except BaseException:
     CUSTOM_CMD = False
 
-CMD_STARTERS = CUSTOM_CMD or ["/", "!"]
+if CUSTOM_CMD:
+    CMD_STARTERS = CUSTOM_CMD
+    LOGGER.debug("Bot custom command handler = \"%s\"", CMD_STARTERS)
+else:
+    CMD_STARTERS = ["/", "!']
 
 
-class AntiSpam:
-    def __init__(self):
-        self.whitelist = (
-            (DEV_USERS or [])
-            + (SUDO_USERS or [])
-            + (SUPPORT_USERS or [])
-            + (WHITELIST_USERS or [])
-        )
-        # Values are HIGHLY experimental, its recommended you pay attention to our commits as we will be adjusting the values over time with what suits best.
-        Duration.CUSTOM = 15  # Custom duration, 15 seconds
-        self.sec_limit = RequestRate(6, Duration.CUSTOM)  # 6 / Per 15 Seconds
-        self.min_limit = RequestRate(20, Duration.MINUTE)  # 20 / Per minute
-        self.hour_limit = RequestRate(100, Duration.HOUR)  # 100 / Per hour
-        self.daily_limit = RequestRate(1000, Duration.DAY)  # 1000 / Per day
-        self.limiter = Limiter(
-            self.sec_limit,
-            self.min_limit,
-            self.hour_limit,
-            self.daily_limit,
-            bucket_class=MemoryListBucket,
-        )
-
-    def check_user(self, user):
-        """
-        Return True if user is to be ignored else False
-        """
-        if user in self.whitelist:
-            return False
-        try:
-            self.limiter.try_acquire(user)
-            return False
-        except BucketFullException:
-            return True
-
-
-SpamChecker = AntiSpam()
-MessageHandlerChecker = AntiSpam()
-
-
-class CustomCommandHandler(CommandHandler):
-    def __init__(self, command, callback, admin_ok=False, allow_edit=False, **kwargs):
+class CustomCommandHandler(tg.CommandHandler):
+    def __init__(self, command, callback, **kwargs):
+        if "admin_ok" in kwargs:
+            del kwargs["admin_ok"]
         super().__init__(command, callback, **kwargs)
 
-        if allow_edit is False:
-            self.filters &= ~(
-                Filters.update.edited_message | Filters.update.edited_channel_post
-            )
-
     def check_update(self, update):
-        if not isinstance(update, Update) or not update.effective_message:
-            return
-        message = update.effective_message
+        if isinstance(update, Update) and update.effective_message:
+            message = update.effective_message
 
-        try:
-            user_id = update.effective_user.id
-        except:
-            user_id = None
-
-        if message.text and len(message.text) > 1:
-            fst_word = message.text.split(None, 1)[0]
-            if len(fst_word) > 1 and any(
-                fst_word.startswith(start) for start in CMD_STARTERS
-            ):
-
-                args = message.text.split()[1:]
-                command = fst_word[1:].split("@")
-                command.append(message.bot.username)
-                if user_id == 1087968824:
-                    user_id = update.effective_chat.id
-                if not (
-                    command[0].lower() in self.command
-                    and command[1].lower() == message.bot.username.lower()
+            if message.text and len(message.text) > 1:
+                fst_word = message.text.split(None, 1)[0]
+                if len(fst_word) > 1 and any(
+                    fst_word.startswith(start) for start in CMD_STARTERS
                 ):
-                    return None
-                if SpamChecker.check_user(user_id):
-                    return None
-                filter_result = self.filters(update)
-                if filter_result:
-                    return args, filter_result
-                else:
-                    return False
-                
-    def handle_update(self, update, dispatcher, check_result, context=None):
-        if context:
-            self.collect_additional_context(context, update, dispatcher, check_result)
-            return self.callback(update, context)
-        else:
-            optional_args = self.collect_optional_args(dispatcher, update, check_result)
-            return self.callback(dispatcher.bot, update, **optional_args)
+                    args = message.text.split()[1:]
+                    command = fst_word[1:].split("@")
+                    command.append(
+                        message.bot.username
+                    )  # in case the command was sent without a username
 
-    def collect_additional_context(self, context, update, dispatcher, check_result):
-        if isinstance(check_result, bool):
-            context.args = update.effective_message.text.split()[1:]
-        else:
-            context.args = check_result[0]
-            if isinstance(check_result[1], dict):
-                context.update(check_result[1])
+                    if not (
+                        command[0].lower() in self.command
+                        and command[1].lower() == message.bot.username.lower()
+                    ):
+                        return None
+
+                    filter_result = self.filters(update)
+                    if filter_result:
+                        return args, filter_result
+                    else:
+                        return False
