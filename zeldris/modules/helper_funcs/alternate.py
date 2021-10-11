599c5c929551f0c823ecf60f286796f39f1e47df
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-10-11 21:23:39 +0700

    Hmm

--- zeldris/modules/helper_funcs/alternate.py
+++ zeldris/modules/helper_funcs/alternate.py
@@ -15,20 +15,17 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+from telegram.error import BadRequest
 from functools import wraps
-
-from telegram import error, ChatAction
-
-from zeldris import LOGGER
+from telegram import ChatAction
 
 
 def send_message(message, text, *args, **kwargs):
     try:
         return message.reply_text(text, *args, **kwargs)
-    except error.BadRequest as err:
+    except BadRequest as err:
         if str(err) == "Reply message not found":
             return message.reply_text(text, quote=False, *args, **kwargs)
-        raise
 
 
 def typing_action(func):
@@ -36,20 +33,10 @@ def typing_action(func):
 
     @wraps(func)
     def command_func(update, context, *args, **kwargs):
-        try:
-            context.bot.send_chat_action(
-                chat_id=update.effective_chat.id, action=ChatAction.TYPING
-            )
-            return func(update, context, *args, **kwargs)
-        except error.BadRequest as err:
-            if str(err) == "Have no rights to send a message":
-                LOGGER.warning("Bot muted in {} {}".format(
-                    update.effective_message.chat.title,
-                    update.effective_message.chat.id
-                )
-                )
-        except error.Unauthorized:
-            return
+        context.bot.send_chat_action(
+            chat_id=update.effective_chat.id, action=ChatAction.TYPING,
+        )
+        return func(update, context, *args, **kwargs)
 
     return command_func
 
