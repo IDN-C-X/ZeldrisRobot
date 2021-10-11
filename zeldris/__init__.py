267dc03c04bc599b88cae2280971f6ced5d4d65d
Ryomen-Sukuna <Ryomen-Sukuna@users.noreply.github.com>
2021-10-11 23:04:10 +0000

    Automated code formatting
    Signed-off-by: Ryomen-Sukuna <80420642+Ryomen-Sukuna@users.noreply.github.com>

--- zeldris/__init__.py
+++ zeldris/__init__.py
@@ -213,7 +213,11 @@ finally:
 
 # Telethon
 client = TelegramClient(MemorySession(), API_ID, API_HASH)
-updater = tg.Updater(TOKEN, workers=min(32, os.cpu_count() + 4), request_kwargs={"read_timeout": 10, "connect_timeout": 10})
+updater = tg.Updater(
+    TOKEN,
+    workers=min(32, os.cpu_count() + 4),
+    request_kwargs={"read_timeout": 10, "connect_timeout": 10},
+)
 dispatcher = updater.dispatcher
 
 DEV_USERS = list(DEV_USERS)
