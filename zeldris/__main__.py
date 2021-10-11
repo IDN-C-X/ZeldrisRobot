382496cdbdfcf9f5d6d73c86c86d7b44bef8f1a1
ZERO • アクバル <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-10-11 18:17:27 +0700

    zeldris: change sending message

--- zeldris/__main__.py
+++ zeldris/__main__.py
@@ -241,9 +241,9 @@ def start(update: Update, context: CallbackContext):
                 timeout=60,
             )
     else:
-        message.reply_animation(
+        message.reply_text(
             ZELDRIS_IMG, 
-            caption="<code>Zeldris Is Here to Maintenance Your Group\n"
+            text="<code>Zeldris Is Here to Maintenance Your Group\n"
                 "I am Awake Since</code>: <code>{}</code>".format(uptime),
             parse_mode=ParseMode.HTML,
             reply_markup=InlineKeyboardMarkup(
