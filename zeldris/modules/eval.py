a75a36f5e6959af991be6b285e9493d0a786827c
ZERO • アクバル <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-10-12 09:56:07 +0700

    zeldris: added eval modules
    Signed-off-by: Ryomen-Sukuna <80420642+Ryomen-Sukuna@users.noreply.github.com>

---
+++ zeldris/modules/eval.py
@@ -0,0 +1,151 @@
+# ZeldrisRobot
+# Copyright (C) 2017-2019, Paul Larsen
+# Copyright (c) 2021, IDNCoderX Team, <https://github.com/IDN-C-X/ZeldrisRobot>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import ast
+import io
+import os
+
+# Common imports for eval
+import textwrap
+import traceback
+from contextlib import redirect_stdout
+
+from zeldris import LOGGER, dispatcher
+from zeldris.modules.helper_funcs.chat_status import dev_plus
+from telegram import ParseMode, Update
+from telegram.ext import CallbackContext, CommandHandler
+
+namespaces = {}
+
+
+def namespace_of(chat, update, bot):
+    if chat not in namespaces:
+        namespaces[chat] = {
+            "__builtins__": globals()["__builtins__"],
+            "bot": bot,
+            "effective_message": update.effective_message,
+            "effective_user": update.effective_user,
+            "effective_chat": update.effective_chat,
+            "update": update,
+        }
+
+    return namespaces[chat]
+
+
+def log_input(update):
+    user = update.effective_user.id
+    chat = update.effective_chat.id
+    LOGGER.info(f"IN: {update.effective_message.text} (user={user}, chat={chat})")
+
+
+def send(msg, bot, update):
+    if len(str(msg)) > 2000:
+        with io.BytesIO(str.encode(msg)) as out_file:
+            out_file.name = "output.txt"
+            bot.send_document(chat_id=update.effective_chat.id, document=out_file)
+    else:
+        LOGGER.info(f"OUT: '{msg}'")
+        bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=f"`{msg}`",
+            parse_mode=ParseMode.MARKDOWN,
+        )
+
+
+@dev_plus
+def evaluate(update: Update, context: CallbackContext):
+    bot = context.bot
+    send(do(eval, bot, update), bot, update)
+
+
+@dev_plus
+def execute(update: Update, context: CallbackContext):
+    bot = context.bot
+    send(do(exec, bot, update), bot, update)
+
+
+def cleanup_code(code):
+    if code.startswith("```") and code.endswith("```"):
+        return "\n".join(code.split("\n")[1:-1])
+    return code.strip("` \n")
+
+
+def do(func, bot, update):
+    log_input(update)
+    content = update.message.text.split(" ", 1)[-1]
+    body = cleanup_code(content)
+    env = namespace_of(update.message.chat_id, update, bot)
+
+    os.chdir(os.getcwd())
+    with open(
+        os.path.join(os.getcwd(), "Cutiepii_Robot/modules/helper_funcs/temp.txt"), "w",
+    ) as temp:
+        temp.write(body)
+
+    stdout = io.StringIO()
+
+    to_compile = f'def func():\n{textwrap.indent(body, "  ")}'
+
+    try:
+        exec(to_compile, env)
+    except Exception as e:
+        return f"{e.__class__.__name__}: {e}"
+
+    func = env["func"]
+
+    try:
+        with redirect_stdout(stdout):
+            func_return = func()
+    except Exception as e:
+        value = stdout.getvalue()
+        return f"{value}{traceback.format_exc()}"
+    else:
+        value = stdout.getvalue()
+        result = None
+        if func_return is None:
+            if value:
+                result = f"{value}"
+            else:
+                try:
+                    result = f"{repr(ast.literal_eval(body, env))}"
+                except:
+                    pass
+        else:
+            result = f"{value}{func_return}"
+        if result:
+            return result
+
+
+@dev_plus
+def clear(update: Update, context: CallbackContext):
+    bot = context.bot
+    log_input(update)
+    global namespaces
+    if update.message.chat_id in namespaces:
+        del namespaces[update.message.chat_id]
+    send("Cleared locals.", bot, update)
+
+
+EVAL_HANDLER = CommandHandler(("e", "ev", "eva", "eval"), evaluate, run_async=True)
+EXEC_HANDLER = CommandHandler(("x", "ex", "exe", "exec", "py"), execute, run_async=True)
+CLEAR_HANDLER = CommandHandler("clearlocals", clear, run_async=True)
+
+dispatcher.add_handler(EVAL_HANDLER)
+dispatcher.add_handler(EXEC_HANDLER)
+dispatcher.add_handler(CLEAR_HANDLER)
+
+__mod_name__ = "Eval Module"
