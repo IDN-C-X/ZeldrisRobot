4741f875c1a2d70c02554fa8a4cf8ee87d1756c5
ZERO • アクバル <80420642+Ryomen-Sukuna@users.noreply.github.com>
2021-11-22 17:41:01 +0700

    afk: use f-string

--- zeldris/modules/afk.py
+++ zeldris/modules/afk.py
@@ -56,7 +56,7 @@ def afk(update, _):
     REDIS.set(f"afk_time_{user.id}", start_afk_time)
     fname = user.first_name
     try:
-        message.reply_text("{} is now AFK!".format(fname))
+        message.reply_text(f"{fname} is now AFK!")
     except BadRequest:
         pass
 
@@ -80,12 +80,11 @@ def no_longer_afk(update, _):
         firstname = update.effective_user.first_name
         try:
             message.reply_text(
-                "{} is back online!\nYou were away for: {}".format(
-                    firstname, end_afk_time
-                )
+                f"{firstname} is back online!\n"
+                f"You were away for: {end_afk_time}"
             )
-        except BaseException:
-            pass
+        except BadRequest:
+            return
 
 
 def reply_afk(update, context):
@@ -123,14 +122,15 @@ def reply_afk(update, context):
 
                 try:
                     chat = context.bot.get_chat(user_id)
-                except BadRequest:
+                except BadRequest as e:
                     print(
-                        "Error: Could not fetch userid {} for AFK module".format(
-                            user_id
+                        "Error: Could not fetch userid {} for AFK module due to {}".format(
+                            user_id, e
                         )
                     )
                     return
-                fst_name = chat.first_name
+
+            fst_name = chat.first_name
 
             else:
                 return
@@ -143,7 +143,7 @@ def reply_afk(update, context):
         check_afk(update, context, user_id, fst_name, userc_id)
 
 
-def check_afk(update, context, user_id: int, fst_name: int, userc_id: int):
+def check_afk(update, _, user_id: int, fst_name: int, userc_id: int):
     if is_user_afk(user_id):
         reason = afk_reason(user_id)
         since_afk = get_readable_time(
@@ -152,10 +152,15 @@ def check_afk(update, context, user_id: int, fst_name: int, userc_id: int):
         if int(userc_id) == int(user_id):
             return
         if reason == "none":
-            res = "{} is AFK!\nLast seen: {}".format(fst_name, since_afk)
+            res = (
+                f"{fst_name} is AFK!\n"
+                f"Last seen: {since_afk}"
+            )
         else:
-            res = "{} is AFK!\nReason: {}\nLast seen: {}".format(
-                fst_name, reason, since_afk
+            res = (
+                f"{fst_name} is AFK!\n"
+                f"Reason: {reason}\n"
+                f"Last seen: {since_afk}"
             )
 
         update.effective_message.reply_text(res)
