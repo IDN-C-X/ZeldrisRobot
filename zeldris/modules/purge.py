9a0de1ed16275e9158d09177e14c68afaa486ca2
ZERO • アクバル <80420642+Ryomen-Sukuna@users.noreply.github.com>
2022-01-16 01:06:12 +0700

    purge: fix admins checker
    Signed-off-by: Ryomen-Sukuna <80420642+Ryomen-Sukuna@users.noreply.github.com>

--- zeldris/modules/purge.py
+++ zeldris/modules/purge.py
@@ -41,7 +41,7 @@ async def purge(event):
     chat = event.chat_id
     msgs = []
 
-    if not await is_administrator(user_id=event.from_id, message=event):
+    if not await is_administrator(user_id=event.sender_id, message=event):
         await event.reply("You're not an admin!")
         return
 
@@ -81,7 +81,7 @@ async def purge(event):
 
 @client.on(events.NewMessage(pattern="^/del$"))
 async def delete_msg(event):
-    if not await is_administrator(user_id=event.from_id, message=event):
+    if not await is_administrator(user_id=event.sender_id, message=event):
         await event.reply("You're not an admin!")
         return
 
