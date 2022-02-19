847e74f09b1955c93ed5841cc67109b111d3bb8e
Furry Chemistry <84753790+aryazakaria01@users.noreply.github.com>
2022-02-19 16:34:47 +0700

    Capslock ajg

--- zeldris/modules/no_sql/__init__.py
+++ zeldris/modules/no_sql/__init__.py
@@ -5,7 +5,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from odmantic import AIOEngine
 from pymongo import MongoClient, collection
 
-from Zeldris import MONGO_DB, MONGO_PORT, MONGO_URI
+from zeldris import MONGO_DB, MONGO_PORT, MONGO_URI
 
 # MongoDB Client
 mongodb = MongoClient(MONGO_URI, 27017)[MONGO_DB]
