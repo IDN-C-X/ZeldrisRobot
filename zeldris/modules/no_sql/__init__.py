61846b7dcc63a2ffdeef7b18c2f68efe2a3e016e
Furry Chemistry <84753790+aryazakaria01@users.noreply.github.com>
2022-02-19 16:34:17 +0700

    Create __init__.py

---
+++ zeldris/modules/no_sql/__init__.py
@@ -0,0 +1,21 @@
+"""MongoDB Database."""
+
+from motor import motor_asyncio
+from motor.motor_asyncio import AsyncIOMotorClient
+from odmantic import AIOEngine
+from pymongo import MongoClient, collection
+
+from Zeldris import MONGO_DB, MONGO_PORT, MONGO_URI
+
+# MongoDB Client
+mongodb = MongoClient(MONGO_URI, 27017)[MONGO_DB]
+motor = motor_asyncio.AsyncIOMotorClient(MONGO_URI)
+db = motor[MONGO_DB]
+engine = AIOEngine(motor, MONGO_DB)
+DB_CLIENT = MongoClient(MONGO_URI)
+_DB = DB_CLIENT["Zeldris"]
+
+
+def get_collection(name: str) -> collection:
+    """Get the collection from database."""
+    return _DB[name]
