b3128e4c64dd417717c6d4487945e41b4604c6c1
Yoga Pranata <zyxdevs@otakufanshare.eu.org>
2022-06-10 07:18:21 +0700

    well for now

--- zeldris/__init__.py
+++ zeldris/__init__.py
@@ -210,11 +210,14 @@ DEV_USERS.add(OWNER_ID)
 
 
 # Pass if SpamWatch token not set.
+"""
 if SPAMWATCH is None:
     spamwtc = None
     LOGGER.warning("[Zeldris] Invalid spamwatch api")
 else:
     spamwtc = spamwatch.Client(SPAMWATCH)
+"""
+spamwtc = None
 
 REDIS = StrictRedis.from_url(REDIS_URL, decode_responses=True)
 try:
